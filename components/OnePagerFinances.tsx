02cf99303cd7742f76802dc45fd4d2ae3d130b84
chris yun <yunchris@gmail.com>
2020-11-04 22:23:01 -0800

    Added updates to Finance Card, including price formatting, fundraisingDetails, and two visual representations for funds raised vs funding goal

--- components/OnePagerFinances.tsx
+++ components/OnePagerFinances.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
-import { Heading } from '@chakra-ui/core';
+import React from "react";
+import {
+  Heading,
+  Text,
+  CircularProgress,
+  CircularProgressLabel,
+  Progress,
+} from "@chakra-ui/core";
 
-import { OnePagerData } from '../model/model';
-import { ContentCard } from './ContentCard';
+import { OnePagerData } from "../model/model";
+import { ContentCard } from "./ContentCard";
 
 type OnePagerFinancesProps = {
   onePagerData: OnePagerData;
@@ -14,30 +20,92 @@ export const OnePagerFinances = ({
   onePagerData,
   isLoading,
 }: OnePagerFinancesProps) => {
+  const fundsRaised = onePagerData.fundsRaisedInStage,
+    fundsGoal = onePagerData.fundraisingStageGoal,
+    percentComplete = Math.floor((fundsRaised / fundsGoal) * 100);
+
   // Format a number to include a dollar sign. This function
   // will be improved as part of task 2.
+
+  // Initially I had used toLocaleString(), but this regex solution seems more
+  // performant. Also toLocaleString was glitchy on Safari during testing. 
+  // Regex code from Stack Overflow:
+  // https://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript
   const formatFinanceNumber = (financeNumber: number) => {
-    return `$${financeNumber}`;
+    let formatted = `${financeNumber}`.replace(/\B(?=(\d{3})+\b)/g, ",");
+    return `$${formatted}`;
+  };
+
+  const progressBar = () => {
+    return (
+      <div>
+        <Text fontSize="sm" marginTop="5px">
+          Purpose: {onePagerData.fundraisingDetails}
+        </Text>
+        <div className="funds-progress">
+          <ProgressText>
+            Raised: {formatFinanceNumber(onePagerData.fundsRaisedInStage)}
+          </ProgressText>
+          <ProgressText>
+            {percentComplete}% of Goal:{" "}
+            {formatFinanceNumber(onePagerData.fundraisingStageGoal)}
+          </ProgressText>
+        </div>
+        <Progress value={percentComplete} hasStripe isAnimated height="32px" />
+      </div>
+    );
+  };
+
+  const pieChart = () => {
+    return (
+      <div className="pie-chart">
+        <CircularProgress value={percentComplete} size="120px" thickness={0.4}>
+          <CircularProgressLabel>{percentComplete}%</CircularProgressLabel>
+        </CircularProgress>
+        <div className="finance-details">
+          <Text fontSize="sm" marginTop="5px">
+            Purpose: {onePagerData.fundraisingDetails}
+          </Text>
+          <SubHeading>
+            Funds Raised: {formatFinanceNumber(fundsRaised)}
+          </SubHeading>
+          <SubHeading>
+            Funding Goal: {formatFinanceNumber(fundsGoal)}
+          </SubHeading>
+        </div>
+      </div>
+    );
   };
 
   return (
-    <ContentCard title='Finances' isLoading={isLoading}>
-      <Heading as='h1' size='lg' marginRight='10px'>
-        Funding Stage: {onePagerData.fundraisingStage}
-      </Heading>
-      <SubHeading>
-        Funds Raised: {formatFinanceNumber(onePagerData.fundsRaisedInStage)}
-      </SubHeading>
-      <SubHeading>
-        Funding Goal: {formatFinanceNumber(onePagerData.fundraisingStageGoal)}
-      </SubHeading>
-    </ContentCard>
+    <div>
+      <ContentCard title="Finances" isLoading={isLoading}>
+        <Heading as="h1" size="lg" marginRight="10px">
+          Funding Stage: {onePagerData.fundraisingStage}
+        </Heading>
+        {progressBar()}
+      </ContentCard>
+
+      {/* Version 2 */}
+      <ContentCard title="Finances Version 2" isLoading={isLoading}>
+        <Heading as="h1" size="lg" marginRight="10px">
+          Funding Stage: {onePagerData.fundraisingStage}
+        </Heading>
+        {pieChart()}
+      </ContentCard>
+    </div>
   );
 };
 
 /** Renders smaller heading. */
 const SubHeading = ({ children }) => (
-  <Heading as='h2' size='md' marginRight='10px'>
+  <Heading as="h2" size="md" marginRight="10px">
+    {children}
+  </Heading>
+);
+
+const ProgressText = ({ children }) => (
+  <Heading as="h3" size="sm" marginRight="10px">
     {children}
   </Heading>
 );
--- components/OnePagerVideo.tsx
+++ components/OnePagerVideo.tsx
@@ -14,29 +14,33 @@ export const OnePagerVideo = ({
   isLoading,
 }: OnePagerVideoProps) => {
 
-  let id, timeStamp, embed, viewPort;
-  // Checks to see if there is a video link and renders null viewPort if not 
-  if (onePagerData.pitchVideoLink) {
-    id = onePagerData.pitchVideoLink.split('?v=')[1].split('&t=');
-    // Opted to use iframe embed for speedy development & proof of concept:
-    if (id.length === 1) embed = `https://youtube.com/embed/${id[0]}`;
-    else {
-    // Used the slice here to remove 's'. Embedded timestamp does not like the 's'
-      timeStamp = id[1].slice(0, id[1].length-1)
-      embed = `https://youtube.com/embed/${id[0]}?start=${timeStamp}`
-    }
-
-    viewPort = 
-    <div>
-      <Diveder50 />
-      <ContentCard title='Pitch Video' isLoading={isLoading}>
-        <iframe width="100%" height="350" src={embed}></iframe>
-      </ContentCard>
-    </div>
-  } else viewPort = null;
+  const viewPort = () => {
+    let id, timeStamp, embed;
+    
+    // Checks to see if there is a video link and returns null viewPort if not 
+    if (onePagerData.pitchVideoLink) {
+      id = onePagerData.pitchVideoLink.split('?v=')[1].split('&t=');
+      // Opted to use iframe embed for speedy development & proof of concept:
+      if (id.length === 1) embed = `https://youtube.com/embed/${id[0]}`;
+      else {
+      // Used the slice here to remove 's'. Embedded timestamp does not like the 's'
+        timeStamp = id[1].slice(0, id[1].length-1)
+        embed = `https://youtube.com/embed/${id[0]}?start=${timeStamp}`
+      };
+  
+      return (
+        <div>
+          <Diveder50 />
+          <ContentCard title='Pitch Video' isLoading={isLoading}>
+            <iframe width="100%" height="350" src={embed}></iframe>
+          </ContentCard>
+        </div>
+      )
+    } else return null;
+  }
 
   return (
-    viewPort
+    viewPort()
   );
 };
 
