4ea113db729a5085ed45bfaf12916fc4aa8bd71b
chris yun <yunchris@gmail.com>
2020-11-07 20:51:25 -0800

    Edited contact info card

--- components/Dashboard/DashboardContact.tsx
+++ components/Dashboard/DashboardContact.tsx
@@ -21,20 +21,22 @@ export const DashboardContact = ({
     if (!dataLoaded) return <LoadingSkeletons />
     else return (
       <>
-        <SimpleGrid columns={2}  w="100%" mb="20px" mt="20px">
+        <Flex justify="space-between" m="20px auto">
           <Flex align="center">
               <Icon name="email" size="24px" color="blue.500" mr="10px" ml="20px" />
-              <SubHeading>Email:</SubHeading>
               <Text color="#545454" m="0">{contact.email}</Text>
           </Flex>
           <Flex align="center">
               <Icon name="phone" size="20px" color="blue.500" mr="10px" ml="20px" />
-              <SubHeading>Phone:</SubHeading>
               <Text color="#545454" m="0">{contact.phoneNum}</Text>
           </Flex>
-        </SimpleGrid>
+          <Flex align="center">
+            <Icon name="link" size="20px" color="blue.500" mr="10px" ml="20px"/>
+            <a href={contact.websiteURL}><SubHeading>Website</SubHeading></a>
+          </Flex>
+        </Flex>
         <Flex align="center" mb="20px">
-          <Icon name="link" size="20px" color="blue.500" mr="10px" ml="20px"/>
+          <Icon name="at-sign" size="20px" color="blue.500" mr="10px" ml="20px"/>
           <SubHeading>Address:</SubHeading>
           <Text color="#545454" m="0">{contact.address}</Text>
         </Flex>
